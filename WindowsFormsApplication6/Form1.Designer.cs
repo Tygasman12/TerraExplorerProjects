f8a138c0f0a6f4b63afb5e9075d07d3d3d98692d
Tygasman12 <tygasman1@hotmail.com>
2016-06-15 13:07:41 -0400

    MissleFiringProject

---
+++ WindowsFormsApplication6/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace WindowsFormsApplication6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(61, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(45, 45);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Forward_);
+            // 
+            // button2
+            // 
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(112, 45);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(45, 45);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Right_);
+            // 
+            // button3
+            // 
+            this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
+            this.button3.Location = new System.Drawing.Point(61, 70);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(45, 45);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Backwards_);
+            // 
+            // button4
+            // 
+            this.button4.Image = ((System.Drawing.Image)(resources.GetObject("button4.Image")));
+            this.button4.Location = new System.Drawing.Point(10, 45);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(45, 45);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Left_);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Location = new System.Drawing.Point(12, 22);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(170, 126);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Distance (X,Y)";
+            // 
+            // button5
+            // 
+            this.button5.Image = ((System.Drawing.Image)(resources.GetObject("button5.Image")));
+            this.button5.Location = new System.Drawing.Point(61, 19);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(45, 45);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.TiltUp_);
+            // 
+            // button6
+            // 
+            this.button6.Image = ((System.Drawing.Image)(resources.GetObject("button6.Image")));
+            this.button6.Location = new System.Drawing.Point(10, 45);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(45, 45);
+            this.button6.TabIndex = 7;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.TiltLeft_);
+            // 
+            // button7
+            // 
+            this.button7.Image = ((System.Drawing.Image)(resources.GetObject("button7.Image")));
+            this.button7.Location = new System.Drawing.Point(112, 45);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(45, 45);
+            this.button7.TabIndex = 5;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.TiltRight_);
+            // 
+            // button8
+            // 
+            this.button8.Image = ((System.Drawing.Image)(resources.GetObject("button8.Image")));
+            this.button8.Location = new System.Drawing.Point(61, 70);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(45, 45);
+            this.button8.TabIndex = 6;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.TiltDown_);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Controls.Add(this.button7);
+            this.groupBox2.Controls.Add(this.button8);
+            this.groupBox2.Controls.Add(this.button6);
+            this.groupBox2.Location = new System.Drawing.Point(12, 154);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(170, 124);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Tilt/Roll";
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Franklin Gothic Medium", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.ForeColor = System.Drawing.Color.Red;
+            this.button9.Location = new System.Drawing.Point(188, 173);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(99, 104);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "FIRE";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.Fire_);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(188, 135);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(99, 32);
+            this.button10.TabIndex = 10;
+            this.button10.Text = "Zoom Out";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.ZoomOut_);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(188, 97);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(99, 32);
+            this.button11.TabIndex = 11;
+            this.button11.Text = "Zoom In";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.ZoomIn_);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(188, 59);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(99, 32);
+            this.button12.TabIndex = 12;
+            this.button12.Text = "Speed Up";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.SpeedUp_);
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(188, 22);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(99, 32);
+            this.button13.TabIndex = 13;
+            this.button13.Text = "Speed Down";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.SpeedDown_);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(297, 291);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+    }
+}
+
---
+++ WindowsFormsApplication6/Form1.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TerraExplorerX;
+
+namespace WindowsFormsApplication6
+{
+
+    public partial class Form1 : Form
+    {
+        private SGWorld65 globe;
+        private SGWorld66 globe1;
+        private DateTime savedTime;
+        private double MoveDistance, TiltDistance;
+        private IPosition65 curPos;
+        private ITerrainModel65 MissleObj;
+        private ITerrainRegularPolygon65 CircleObj;
+        private ITerrainEffect66 explo;
+        private _ISGWorld65Events_OnFrameEventHandler myOnFrameEventHandler;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            globe = new SGWorld65();
+            globe1 = new SGWorld66();
+
+            MoveDistance = 250;
+            TiltDistance = 5;
+
+            myOnFrameEventHandler = new _ISGWorld65Events_OnFrameEventHandler(TE_OnFrame);
+
+        }
+        private const string EffectXML = @"$$PARTICLE$$UserDefine: 
+                     <?xml version='1.0' encoding='UTF-8'?> 
+                     <Particle ID='Custom'><ParticleEmitter ID='ring' NumParticles='575' Texture='WhiteSmokeLight.png'>
+                    <Emitter Rate='602' Shape='Sphere' SpeedShape='Sphere' Scale='0,0,0' Speed='1,0.51,1' />
+                    <Cycle Value='1' />
+                    <Sort Value='1' />
+                    <Rotation Speed='2' Time='1' Initial='0' />
+                    <Render Value='Billboard' />
+                    <Gravity Value='0, 0, 0' />
+                    <Force Value='0' OverrideRotation='0' />
+                    <Position Value='0, 0, 0' />
+                    <Life Value='6.25' />
+                    <Speed Value='3.28' />
+                    <Color Value='20,200,170,160' />
+                    <Size Value='4,4' />
+                    <Drag Value='0' />
+                    <Blend Type='' />
+                    <Fade FadeIn='0.13' FadeOut='0.06' MaxFade='0.17' />
+                    </ParticleEmitter>
+                    <ParticleEmitter ID='ring' NumParticles='572' Texture='Dust.png'>
+                    <Emitter Rate='602' Shape='Sphere' SpeedShape='Sphere' Scale='0,0,0' Speed='1,0.76,1' />
+                    <Cycle Value='1' />
+                    <Sort Value='1' />
+                    <Rotation Speed='2' Time='3' Initial='1' />
+                    <Render Value='Billboard' />
+                    <Gravity Value='0, -1, 0' />
+                    <Force Value='0' OverrideRotation='0' />
+                    <Position Value='0, 0, 0' />
+                    <Life Value='6.25' />
+                    <Speed Value='3.28' />
+                    <Color Value='20,210,170,145' />
+                    <Size Value='4.1,4.1' />
+                    <Drag Value='0' />
+                    <Blend Type='' />
+                    <Fade FadeIn='0.22' FadeOut='0.3' MaxFade='0.14' />
+                    </ParticleEmitter>
+                    <ParticleEmitter ID='ring' NumParticles='26' Texture='fire.png'>
+                    <Emitter Rate='597' Shape='Disc' SpeedShape='Disc' Scale='0,0,0' Speed='1,1,1' />
+                    <Cycle Value='1' />
+                    <Sort Value='1' />
+                    <Render Value='Billboard' />
+                    <Gravity Value='0, -1, 0' />
+                    <Force Value='0' OverrideRotation='0' />
+                    <Position Value='0, 0, 0' />
+                    <Life Value='6.25' />
+                    <Speed Value='1.56' />
+                    <Color Value='20,128,128,255' />
+                    <Size Value='3.3,3.3' />
+                    <Drag Value='1' />
+                    <Blend Type='Add' />
+                    <Fade FadeIn='0.2' FadeOut='0' MaxFade='0.8' />
+                    </ParticleEmitter>
+                    <ParticleEmitter ID='ring' NumParticles='572' Texture='Dust.png'>
+                    <Emitter Rate='602' Shape='Cone' SpeedShape='Cone' Scale='0,1,0' Speed='1,1,1' />
+                    <Cycle Value='1' />
+                    <Sort Value='1' />
+                    <Rotation Speed='1' Time='3' Initial='1' />
+                    <Render Value='Billboard' />
+                    <Gravity Value='0, -1, 0' />
+                    <Force Value='0' OverrideRotation='0' />
+                    <Position Value='0, 0, 0' />
+                    <Life Value='6.25' />
+                    <Speed Value='3.75' />
+                    <Color Value='20,210,170,145' />
+                    <Size Value='4.1,4.1' />
+                    <Drag Value='0' />
+                    <Blend Type='' />
+                    <Fade FadeIn='0.22' FadeOut='0.3' MaxFade='0.13' />
+                    </ParticleEmitter>
+                    </Particle>";
+
+        private void TE_OnFrame()
+        {
+
+
+
+
+            var wpt = globe.Window.CenterPixelToWorld(WorldPointType.WPT_TERRAIN).Position;
+            var wpt1 = globe1.Window.CenterPixelToWorld(WorldPointType.WPT_TERRAIN).Position;
+
+            DateTime now = DateTime.Now;
+            TimeSpan timeDifference = now.Subtract(savedTime);
+            savedTime = now;
+
+            double ftPerSecond = 500.0;
+
+            double distance = ftPerSecond * (timeDifference.Milliseconds / 1000.0);
+
+            IPosition65 newPosition = MissleObj.Position.Move(distance, curPos.Yaw, curPos.Pitch);
+            MissleObj.Position = newPosition;
+            if (MissleObj.Position.AltitudeType == AltitudeTypeCode.ATC_TERRAIN_ABSOLUTE)
+            {
+                IPosition65 clone = newPosition.Copy();
+                clone = clone.ToRelative(AccuracyLevel.ACCURACY_NORMAL);
+                if (clone.Altitude <= 0.0)
+                {
+                    explo = globe1.Creator.CreateEffect(wpt1, EffectsXML = EffectXML, "", "Explosion");
+                    CircleObj = globe.Creator.CreateCircle(wpt, 15.0, LineColor = Color.Red, FillColor = Color.Red, "", "Circle");
+                    globe.OnFrame -= myOnFrameEventHandler;
+                }
+            }
+
+        }
+
+        
+
+       
+
+
+
+
+
+        private void Forward_(object sender, EventArgs e)
+        {
+            IPosition65 newPos;
+            curPos = globe.Navigate.GetPosition(AltitudeTypeCode.ATC_TERRAIN_ABSOLUTE);
+            newPos = curPos.Move(MoveDistance, curPos.Yaw, 0);
+            newPos.Yaw = curPos.Yaw;
+            newPos.Pitch = curPos.Pitch;
+            globe.Navigate.SetPosition(newPos);
+        }
+
+        private void Right_(object sender, EventArgs e)
+        {
+            IPosition65 newPos;
+            curPos = globe.Navigate.GetPosition(AltitudeTypeCode.ATC_TERRAIN_ABSOLUTE);
+            newPos = curPos.Move(MoveDistance, curPos.Yaw + 90, 0);
+            newPos.Yaw = curPos.Yaw;
+            newPos.Pitch = curPos.Pitch;
+            globe.Navigate.SetPosition(newPos);
+        }
+
+        private void Backwards_(object sender, EventArgs e)
+        {
+            IPosition65 newPos;
+            curPos = globe.Navigate.GetPosition(AltitudeTypeCode.ATC_TERRAIN_ABSOLUTE);
+            newPos = curPos.Move(-MoveDistance, curPos.Yaw, 0);
+            newPos.Yaw = curPos.Yaw;
+            newPos.Pitch = curPos.Pitch;
+            globe.Navigate.SetPosition(newPos);
+        }
+
+        private void Left_(object sender, EventArgs e)
+        {
+            IPosition65 newPos;
+            curPos = globe.Navigate.GetPosition(AltitudeTypeCode.ATC_TERRAIN_ABSOLUTE);
+            newPos = curPos.Move(MoveDistance, curPos.Yaw - 90, 0);
+            newPos.Yaw = curPos.Yaw;
+            newPos.Pitch = curPos.Pitch;
+            globe.Navigate.SetPosition(newPos);
+        }
+
+        private void TiltUp_(object sender, EventArgs e)
+        {
+            curPos = globe.Navigate.GetPosition(AltitudeTypeCode.ATC_TERRAIN_ABSOLUTE);
+            curPos.Pitch += TiltDistance;
+            globe.Navigate.SetPosition(curPos);
+        }
+
+        private void TiltRight_(object sender, EventArgs e)
+        {
+            curPos = globe.Navigate.GetPosition(AltitudeTypeCode.ATC_TERRAIN_ABSOLUTE);
+            curPos.Yaw += TiltDistance;
+            globe.Navigate.SetPosition(curPos);
+        }
+
+        private void TiltDown_(object sender, EventArgs e)
+        {
+            curPos = globe.Navigate.GetPosition(AltitudeTypeCode.ATC_TERRAIN_ABSOLUTE);
+            curPos.Pitch -= TiltDistance;
+            globe.Navigate.SetPosition(curPos);
+        }
+
+        private void TiltLeft_(object sender, EventArgs e)
+        {
+            curPos = globe.Navigate.GetPosition(AltitudeTypeCode.ATC_TERRAIN_ABSOLUTE);
+            curPos.Yaw -= TiltDistance;
+            globe.Navigate.SetPosition(curPos);
+        }
+
+        private void Fire_(object sender, EventArgs e)
+        {
+            curPos = globe.Navigate.GetPosition(AltitudeTypeCode.ATC_TERRAIN_ABSOLUTE);
+
+
+            var wpt = globe.Window.CenterPixelToWorld().Position;
+            var wpt1 = globe1.Window.CenterPixelToWorld().Position;
+            string FullPathName;
+
+            FullPathName = @"C:\Program Files (x86)\Skyline\TerraExplorer Pro\Tools\Data-Library\3D-Objects\AirPlanes\f15.xpc";
+            MissleObj = globe.Creator.CreateModel(curPos, FullPathName, 0.5, ModelTypeCode.MT_NORMAL, "", "Missle");
+
+            savedTime = DateTime.Now;
+
+
+            globe.OnFrame += myOnFrameEventHandler;
+        }
+        public Color LineColor { get; set; }
+
+        public Color FillColor { get; set; }
+
+        public string EffectsXML { get; set; }
+
+       
+        private void ZoomOut_(object sender, EventArgs e)
+        {
+            curPos = globe.Navigate.GetPosition(AltitudeTypeCode.ATC_TERRAIN_ABSOLUTE);
+            globe.Navigate.SetPosition(curPos.Move(-MoveDistance, curPos.Yaw, curPos.Pitch));
+        }
+
+        private void ZoomIn_(object sender, EventArgs e)
+        {
+            curPos = globe.Navigate.GetPosition(AltitudeTypeCode.ATC_TERRAIN_ABSOLUTE);
+            globe.Navigate.SetPosition(curPos.Move(MoveDistance, curPos.Yaw, curPos.Pitch));
+        }
+
+        private void SpeedUp_(object sender, EventArgs e)
+        {
+            MoveDistance = MoveDistance + 25;
+        }
+
+        private void SpeedDown_(object sender, EventArgs e)
+        {
+            MoveDistance = MoveDistance - 25;
+
+        }
+
+
+    }
+}
